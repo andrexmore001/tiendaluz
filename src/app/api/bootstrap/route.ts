a582cebf7da26bbb2d74a3d8ccc9c72bb17a6f54
Andres Leonardo Moreno Melo <andresmore001@gmail.com>
2026-03-10 16:37:12 -0500

    Despliegue

--- src/app/api/bootstrap/route.ts
+++ src/app/api/bootstrap/route.ts
@@ -3,7 +3,7 @@ import { revalidatePath } from 'next/cache';
 import prisma from '@/lib/prisma';
 import { auth } from '@/auth';
 
-export const revalidate = 3600; // Cache for 1 hour
+export const dynamic = 'force-dynamic';
 
 export async function GET() {
     try {
